b8a9484f7a1a28d56c4aedb39c5b6d9fdcdc5b97
fajrul aulia <fajrul.aulia@citra.digital>
2022-01-24 01:54:35 +0700

    update

--- src/services/config.js
+++ src/services/config.js
@@ -2,8 +2,8 @@ import Axios from 'axios'
 
 
 const Config = {
-    HOSTNAME: "http://192.168.15.150:3000",
-    WEBSOCKET: "ws://192.168.15.150:3000",
+    HOSTNAME: "https://coreplause-webservice.herokuapp.com",
+    WEBSOCKET: "wss://coreplause-webservice.herokuapp.com",
 
     testConnect: () => {
         return Axios.get(Config.HOSTNAME)
